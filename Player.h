f46837bdf0fce5342295b5b705a5a0179d57880e
Alex Olinger <alexolingervideo@gmail.com>
2021-03-04 01:20:14 -0500

    hook up Player class to Subject class for Observers to see

--- Player.h
+++ Player.h
@@ -2,10 +2,15 @@
 #include "Observer.h"
 
 class Player : public Subject
+{
 public:
 	void Hit();
 	void PickUpHealth();
-	int GetLife() const { int life };
+	int GetTotalHealth() const { return totalHealth; }
+	int GetCurrentHealth() const { return currentHealth; }
 
 private:
-	const int initialLife;
+	const int totalHealth;
+	int currentHealth;
+
+};
